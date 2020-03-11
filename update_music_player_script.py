d10180d55676864971c455582ddb00ef219175ff
scriptslay3r <36932677+scriptslay3r@users.noreply.github.com>
2020-03-11 15:03:34 -0500

    Repositioned and Resized Buttons and windows

--- update_music_player_script.py
+++ update_music_player_script.py
@@ -5,11 +5,17 @@ import pygame
 
 """ my name is yogesh singh , Admin og instagram page dynamic_codeing ....
  you can follow me on github and instagram for more amazing python projects ..."""
+
+"""directory = input("Where is your music located?")
+maybe add an option to change music directory"""
+
+
+
 class MusicPlayer:
     def __init__(self,root):
         self.root = root
         self.root.title("Music-Player")
-        self.root.geometry("697x250+300+220")
+        self.root.geometry("1024x600")
         self.root.resizable(0,0)
         # initializing pygame constructor ....
         pygame.init()
@@ -31,62 +37,60 @@ class MusicPlayer:
 
         # title frame
         track_frame = LabelFrame(self.root,text="Song Track",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-        track_frame.place(x=0,y=0,width=400,height=100)
+        track_frame.place(x=0,y=0,width=1024,height=100)
 
         # Inserting Song Track Label
         songtrack = Label(track_frame, textvariable=self.track, width=15, font=("times new roman", 20, "bold"),
                           bg="grey", fg="gold")
-        songtrack.grid(row=0, column=0, ipadx=5, pady=5)
+        songtrack.place(x=0,y=5,width=1000,height=35)
         # Inserting Status Label
         trackstatus = Label(track_frame, textvariable=self.status, font=("times new roman", 15, "bold"), bg="grey",fg="gold")
-        trackstatus.grid(row=0, column=1, padx=5, pady=5)
+        trackstatus.place(x=900,y=35,width=100,height=25)
 
         # Creating Button Frame
         buttonframe = LabelFrame(self.root, text="Control Panel", font=("times new roman", 15, "bold"), bg="grey",
                                  fg="white", bd=5, relief=GROOVE)
-        buttonframe.place(x=0, y=100, width=400, height=150)
+        buttonframe.place(x=0, y=100, width=1024, height=200)
         # Inserting Play Button
         playbtn = Button(buttonframe, text="PLAY", command=self.playsong, width=6, height=1,
-                         font=("times new roman", 12, "bold"), fg="navyblue", bg="gold").grid(row=0, column=0, padx=5,
-                                                                                              pady=5)
+                         font=("times new roman", 12, "bold"), fg="navyblue", bg="gold").place(x=5,y=5)
         # Inserting Pause Button
         self.pausebtn = Button(buttonframe, text="PAUSE", command=self.pausesong, width=10, height=1,
                          font=("times new roman", 12, "bold"), fg="navyblue", bg="gold")
-        self.pausebtn.grid(row=0, column=1, padx=5, pady=5)
-
+        self.pausebtn.place(x=460,y=5)
 
         # Inserting Stop Button
         stopbtn = Button(buttonframe, text="STOP", command=self.stopsong, width=6, height=1,
-                         font=("times new roman", 12, "bold"), fg="navyblue", bg="gold").grid(row=0, column=3, padx=5,
-                                                                                              pady=5)
+                         font=("times new roman", 12, "bold"), fg="navyblue", bg="gold").place(x=947,y=5)
         # inserting volume slider ...
 
-        self.scale = Scale(buttonframe,orient='horizontal', from_=0, to=100, command=self.setvolume,length =280,relief = GROOVE,bg='grey',troughcolor="gold",
+        self.scale = Scale(buttonframe,orient='horizontal', from_=0, to=100, command=self.setvolume,length =1000,relief = GROOVE,bg='grey',troughcolor="gold",
                       fg="Gold",sliderlength=10,width=15)
         self.scale.set(50)
-        self.scale.grid(row=1,columnspan=3,padx=10,pady=5)
-
-        # Inserting Stop Button
-        openbtn = Button(buttonframe, text="Open", command=self.open, width=6, height=1,
-                         font=("times new roman", 14, "bold"), fg="navyblue", bg="gold").grid(row=1, column=3, padx=5,pady=5)
+        self.scale.place(x=5,y=75)
 
         # Creating Playlist Frame
         songsframe = LabelFrame(self.root, text="Song Playlist", font=("times new roman", 15, "bold"), bg="grey",
                                 fg="white", bd=5, relief=GROOVE)
-        songsframe.place(x=400, y=0, width=300, height=250)
+        songsframe.place(x=0, y=300, width=1024, height=300)
+
+                # Inserting Open Button
+        openbtn = Button(songsframe, text="Open", command=self.open, width=6, height=1,
+                         font=("times new roman", 14, "bold"), fg="navyblue", bg="gold").pack(side=BOTTOM)
+
         # Inserting scrollbar
         scrol_y = Scrollbar(songsframe, orient=VERTICAL)
         # Inserting Playlist listbox
         self.playlist = Listbox(songsframe, yscrollcommand=scrol_y.set, selectbackground="gold", selectmode=SINGLE,
                                 font=("times new roman", 12, "bold"), bg="silver", fg="navyblue", bd=5, relief=GROOVE)
+        self.playlist.place(x=512, y=0, width=500, height=575)
         # Applying Scrollbar to listbox
         scrol_y.pack(side=RIGHT, fill=Y)
         scrol_y.config(command=self.playlist.yview)
         self.playlist.pack(fill=BOTH)
 
         # Changing Directory for fetching Songs
-        os.chdir("C:\\Users\\Yogesh\\Desktop\\internet explorer\\pchjd")
-
+        os.chdir("C:\\Users\\brannon.harper\\Music")
         # Fetching Songs
         songtracks = os.listdir()
         # Inserting Songs into Playlist
